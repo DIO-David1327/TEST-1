4b9fa69d2c79a29d5c91a0d15c3b52e14f520f89
David1327 <53765374+DIO-David1327@users.noreply.github.com>
2021-09-16 18:32:09 +0200

    Update error.php

--- error.php
+++ error.php
@@ -1,1 +1,23 @@
+data: {error: e.stack.replace(/'/g, '"'), "function": fn, browser: getBrowser(), version: version},
 
+data: {
+                           'rumeur' : rumeur
+                   },
+
+<?php
+
+$error = urldecode($_POST[error]);
+$function = urldecode($_POST["function"]);
+$browser = urldecode($_POST[browser]);
+$version = urldecode($_POST[version]);
+
+$query = $pdo->prepare('SELECT * FROM rumeur, ville WHERE rumeur.id_ville = ville.id ORDER BY rumeur.id_rumeur DESC LIMIT 5');
+$query->execute();
+$result = $query->fetchAll();
+
+echo json_encode($result);
+
+// foreach($result as $results) {                        
+// echo $results['rumeur'];
+// }
+?>
